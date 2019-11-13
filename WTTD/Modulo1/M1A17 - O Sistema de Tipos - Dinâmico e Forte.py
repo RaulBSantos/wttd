5d6884098252fea8abfcfe304c5a05178f0a4c48
Raul Santos <raulbolsantos@gmail.com>
2019-11-13 18:46:32 -0300

    JupyterNotebook Modulo 1

---
+++ WTTD/Modulo1/M1A17 - O Sistema de Tipos - Dinâmico e Forte.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Testando: Por que o erro informado é NotImplemented quando iniciado de um int?
+
+# In[1]:
+
+
+int('1').__add__('1')
+
+
+# Tentando com str:
+
+# In[2]:
+
+
+str(1).__add__(1)
+
